631e13b8aa8ab1a98c7e143534293efe09a8ad4c
deepika <maligadeepika@gmail.com>
2026-03-06 21:29:04 +0530

    update message

---
+++ Day4.py
@@ -0,0 +1,22 @@
+age = 20
+name = "deepika"
+print("My name is", name, "and my age is", age)
+
+
+x = 5
+y = 10
+x, y = y, x
+print("x=", x)
+print("y=", y)
+
+
+a = 7
+b = 8
+sum = a + b
+print("the sum is:",sum )
+
+x = 3 
+y = 4
+z = x * y
+print(z)
+
