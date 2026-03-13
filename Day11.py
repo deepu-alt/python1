58e03b81f9f45402d5345a432ca6bcc1465d3f36
deepika <maligadeepika@gmail.com>
2026-03-13 19:52:21 +0530

    update code

---
+++ Day11.py
@@ -0,0 +1,38 @@
+#function with keywords
+def fun(name, course, branch, per):
+    print("hi I'am", name, course, branch, "with", per,"%")
+fun(name="deepika", course= "b tech", branch="AIML", per=80)
+
+# prime numbers without using functions
+start = int(input("enter the starting number:"))
+end = int(input("eneter the ending number:"))
+print("prime numbers between", start, "and", end, "are:")
+
+for num in range(start, end + 1):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
+# function to check prime number
+def check_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+# function to print prime numbers in range
+def find_primes(start, end):
+    for num in range(start, end + 1):
+        if check_prime(num):
+            print(num)
+
+# input from user
+start = int(input("Enter starting number: "))
+end = int(input("Enter ending number: "))
+
+print("Prime numbers are:")
+find_primes(start, end)
