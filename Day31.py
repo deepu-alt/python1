e740dfa11de2dab2cc916ebf10797c276645fb59
deepika <maligadeepika@gmail.com>
2026-04-12 21:40:01 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -30,3 +30,27 @@ class Solution:
                         dp[i][j] = dp[i][j] or dp[i - 1][j]
         
         return dp[m][n]
+
+# container with most water
+class Solution:
+    def maxArea(self, height):
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+        
+        while left < right:
+            # Calculate current area
+            width = right - left
+            h = min(height[left], height[right])
+            area = width * h
+            
+            # Update max area
+            max_area = max(max_area, area)
+            
+            # Move the pointer with smaller height
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        
+        return max_area        
