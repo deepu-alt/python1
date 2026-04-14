1e28e78a4505a2c428f23db818b8057a35827144
deepika <maligadeepika@gmail.com>
2026-04-14 09:34:08 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -84,3 +84,19 @@ class Solution:
                 num -= value
                 
         return result
+
+#find longest common factor
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        
+        prefix = strs[0]
+        
+        for s in strs[1:]:
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]
+                if prefix == "":
+                    return ""
+        
+        return prefix
