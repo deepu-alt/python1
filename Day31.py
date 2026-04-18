c3de260ad844d1ad2ef3fd233e6eafeef645fce1
deepika <maligadeepika@gmail.com>
2026-04-18 20:29:41 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -158,3 +158,28 @@ class Solution:
                 else:
                     right -= 1
         return result
+
+# letter combinations of a phone number
+class Solution:
+    def letterCombinations(self, digits: str):
+        if not digits:
+            return []
+
+        phone = {
+            "2": "abc", "3": "def", "4": "ghi",
+            "5": "jkl", "6": "mno", "7": "pqrs",
+            "8": "tuv", "9": "wxyz"
+        }
+
+        result = []
+
+        def backtrack(index, path):
+            if index == len(digits):
+                result.append(path)
+                return
+            
+            for letter in phone[digits[index]]:
+                backtrack(index + 1, path + letter)
+
+        backtrack(0, "")
+        return result        
