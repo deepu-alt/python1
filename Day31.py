4fed6bace63960b1041226c1e5b02a57db0da12b
deepika <maligadeepika@gmail.com>
2026-04-20 06:03:34 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -226,4 +226,33 @@ class Solution:
                     else:
                         right -= 1
 
-        return result        
+        return result
+
+
+#remove Nth node from end of list
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0)
+        dummy.next = head
+        
+        fast = dummy
+        slow = dummy
+        
+        # Move fast pointer n+1 steps ahead
+        for _ in range(n + 1):
+            fast = fast.next
+        
+        # Move both pointers
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        
+        # Remove nth node
+        slow.next = slow.next.next
+        
+        return dummy.next                
