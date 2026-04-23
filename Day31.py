dcf7a2b7ee96685c0d57a326ec4d809de0701a62
deepika <maligadeepika@gmail.com>
2026-04-23 21:20:41 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -308,4 +308,22 @@ class Solution:
             current.next = list2
         
         return dummy.next
-                    
+
+#generate peranthesis
+class Solution:
+    def generateParenthesis(self, n: int):
+        result = []
+        
+        def backtrack(s, open_count, close_count):
+            if len(s) == 2 * n:
+                result.append(s)
+                return
+            
+            if open_count < n:
+                backtrack(s + "(", open_count + 1, close_count)
+            
+            if close_count < open_count:
+                backtrack(s + ")", open_count, close_count + 1)
+        
+        backtrack("", 0, 0)
+        return result                
