5524e1cbc31e60b30d4665c28d568aa8ba61f2d3
deepika <maligadeepika@gmail.com>
2026-04-24 09:34:11 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -326,4 +326,36 @@ class Solution:
                 backtrack(s + ")", open_count, close_count + 1)
         
         backtrack("", 0, 0)
-        return result                
+        return result
+
+#merge k sorted lists
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+import heapq
+
+class Solution:
+    def mergeKLists(self, lists):
+        heap = []
+        
+        # Push initial nodes into heap
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i, lists[i]))
+        
+        dummy = ListNode(0)
+        current = dummy
+        
+        while heap:
+            val, i, node = heapq.heappop(heap)
+            
+            current.next = node
+            current = current.next
+            
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+        
+        return dummy.next                
