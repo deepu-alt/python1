d4fc89d156d3add6ef19776a431de44f6e47a8d2
deepika <maligadeepika@gmail.com>
2026-04-27 10:41:14 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -425,3 +425,18 @@ class Solution:
         
         # If less than k nodes, return head as it is
         return head
+
+# remove duplicates from sorted array
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        i = 0
+        
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        
+        return i + 1
