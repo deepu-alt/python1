09d06811502aa8574296c11c37838baabb6ff2f6
deepika <maligadeepika@gmail.com>
2026-05-03 19:55:14 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -585,3 +585,16 @@ class Solution:
                     right = mid - 1
         
         return -1
+
+
+#remove elements
+class Solution:
+    def removeElement(self, nums, val):
+        k = 0
+        
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        
+        return k
