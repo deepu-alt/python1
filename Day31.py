52a4cef15b99e453ce9a7c0d62091b5b721c12b4
deepika <maligadeepika@gmail.com>
2026-05-04 17:42:44 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -602,3 +602,43 @@ class Solution:
                 k += 1
         
         return k
+
+#search in rotated sorted array
+class Solution(object):
+    def searchRange(self, nums, target):
+        
+        def findFirst(nums, target):
+            left, right = 0, len(nums) - 1
+            first = -1
+
+            while left <= right:
+                mid = (left + right) // 2
+                
+                if nums[mid] == target:
+                    first = mid 
+                    right = mid - 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
+            return first
+
+        def findLast(nums, target):
+            left, right = 0, len(nums) - 1
+            last = -1
+            
+            while left <= right:
+                mid = (left + right) // 2
+                
+                if nums[mid] == target:
+                    last = mid
+                    left = mid + 1
+                elif nums[mid] < target:   # ✅ fixed
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
+            return last
+
+        return [findFirst(nums, target), findLast(nums, target)]
