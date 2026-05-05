b3aabd8c876681d2ef94a0d9962f93d1c91670de
deepika <maligadeepika@gmail.com>
2026-05-05 19:28:32 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -642,3 +642,20 @@ class Solution(object):
             return last
 
         return [findFirst(nums, target), findLast(nums, target)]
+
+#search insert position
+class Solution(object):
+    def searchInsert(self, nums, target):
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return left
