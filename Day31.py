19d03030b70e22c38898c41303ab0e304823ae19
deepika <maligadeepika@gmail.com>
2026-05-06 17:25:20 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -659,3 +659,23 @@ class Solution(object):
                 right = mid - 1
         
         return left
+
+#valid sudoku
+class Solution:
+    def isValidSudoko(self, board):
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for in range(9):
+            for j in range(9):
+                val = board[i][j]
+                if val == '.':
+                    continue
+                box = (i // 3) * 3 + (j // 3)
+                if val in rows[i] or val in cols[j] or val in boxes[box]:
+                    return False
+                rows[i].add(val)
+                cols[j].add(val)
+                boxes[box].add(val)
+                return True 
