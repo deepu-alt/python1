6dcad2b9347141e58fc1cf35843ca3510fbd27b7
deepika <maligadeepika@gmail.com>
2026-05-13 20:51:14 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -843,3 +843,26 @@ class Solution:
                 trapped_water += max(0, right_max - height[right])
         
         return trapped_water
+
+# multiply strings
+class Solution:
+    def multiply(self, num1, num2):
+        if num1 == "0" or num2 == "0":
+            return "0"
+        
+        m, n = len(num1), len(num2)
+        result = [0] * (m + n)
+        
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                mul = int(num1[i]) * int(num2[j])
+                sum_ = mul + result[i + j + 1]
+                
+                result[i + j + 1] = sum_ % 10
+                result[i + j] += sum_ // 10
+        
+        # Convert result to string
+        result_str = ''.join(map(str, result)).lstrip('0')
+        
+        return result_str if result_str else "0"
+    
