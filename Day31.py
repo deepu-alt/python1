efddc45b499a0352903392be305e9755bad86ead
deepika <maligadeepika@gmail.com>
2026-05-16 16:31:05 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -907,3 +907,19 @@ class Solution:
                 current_end = farthest
         
         return jumps
+
+# permutations
+class Solution:
+    def permute(self, nums):
+        result = []
+        self.backtrack(nums, [], result)
+        return result
+
+    def backtrack(self, nums, path, result):
+        if not nums:
+            result.append(path)
+            return
+        
+        for i in range(len(nums)):
+            self.backtrack(nums[:i] + nums[i+1:], path + [nums[i]], result)
+            
