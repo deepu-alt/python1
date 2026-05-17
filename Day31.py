fc4c05ed2a61893d7a3a3d61d40e60999b3e3cb6
deepika <maligadeepika@gmail.com>
2026-05-17 18:11:09 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -922,4 +922,23 @@ class Solution:
         
         for i in range(len(nums)):
             self.backtrack(nums[:i] + nums[i+1:], path + [nums[i]], result)
+
+#permutations 2
+class Solution:
+    def permuteUnique(self, nums):
+        result = []
+        nums.sort()
+        self.backtrack(nums, [], result)
+        return result
+
+    def backtrack(self, nums, path, result):
+        if not nums:
+            result.append(path)
+            return
+        
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            self.backtrack(nums[:i] + nums[i+1:], path + [nums[i]], result)           
+
             
