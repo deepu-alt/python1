e253cea1f5b93d9f2929b8b8f03d4abe9179ad7d
deepika <maligadeepika@gmail.com>
2026-05-18 20:19:22 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -941,4 +941,16 @@ class Solution:
                 continue
             self.backtrack(nums[:i] + nums[i+1:], path + [nums[i]], result)           
 
-            
+#rotate image
+class Solution:
+    def rotate(self, matrix):
+        n = len(matrix)
+        
+        # Transpose
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        # Reverse each row
+        for i in range(n):
+            matrix[i].reverse()
