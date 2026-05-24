71a4ee8755f8141dd824966e1285a7a115ec6b44
deepika <maligadeepika@gmail.com>
2026-05-24 19:53:34 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -1067,3 +1067,37 @@ class Solution:
                 max_global = max_current
         
         return max_global
+#spiral matrix
+class Solution:
+    def spiralOrder(self, matrix):
+        if not matrix:
+            return []
+        
+        result = []
+        top, bottom = 0, len(matrix) - 1
+        left, right = 0, len(matrix[0]) - 1
+        
+        while top <= bottom and left <= right:
+            # Traverse from left to right
+            for j in range(left, right + 1):
+                result.append(matrix[top][j])
+            top += 1
+            
+            # Traverse from top to bottom
+            for i in range(top, bottom + 1):
+                result.append(matrix[i][right])
+            right -= 1
+            
+            if top <= bottom:
+                # Traverse from right to left
+                for j in range(right, left - 1, -1):
+                    result.append(matrix[bottom][j])
+                bottom -= 1
+            
+            if left <= right:
+                # Traverse from bottom to top
+                for i in range(bottom, top - 1, -1):
+                    result.append(matrix[i][left])
+                left += 1
+        
+        return result
