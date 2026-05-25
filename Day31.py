3a0e15b647932f9bb75c7a1e6c23171c210c40c4
deepika <maligadeepika@gmail.com>
2026-05-25 19:55:56 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -1101,3 +1101,15 @@ class Solution:
                 left += 1
         
         return result
+# jump game
+class Solution:
+    def canJump(self, nums):
+        max_reach = 0
+        
+        for i in range(len(nums)):
+            if i > max_reach:
+                return False
+            max_reach = max(max_reach, i + nums[i])
+        
+        return True
+    
