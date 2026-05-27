14e16677099b0a6ec927c3725dd508cfef967723
deepika <maligadeepika@gmail.com>
2026-05-27 19:38:35 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -1128,4 +1128,20 @@ class Solution:
                 grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
         
         return grid[-1][-1]
-    
+#merge intervals
+class Solution:
+    def merge(self, intervals):
+        if not intervals:
+            return []
+        
+        intervals.sort(key=lambda x: x[0])
+        merged = [intervals[0]]
+        
+        for current in intervals[1:]:
+            last = merged[-1]
+            if current[0] <= last[1]:  # Overlap
+                last[1] = max(last[1], current[1])  # Merge
+            else:
+                merged.append(current)  # No overlap
+        
+        return merged
