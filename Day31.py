8db7ba1cf581264430dd3f27cb82a2d7f2cc98d1
deepika <maligadeepika@gmail.com>
2026-05-28 18:48:05 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -1145,3 +1145,29 @@ class Solution:
                 merged.append(current)  # No overlap
         
         return merged
+# insert intervals
+class Solution:
+    def insert(self, intervals, newInterval):
+        result = []
+        i = 0
+        n = len(intervals)
+        
+        # Add all intervals before newInterval
+        while i < n and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i += 1
+        
+        # Merge overlapping intervals with newInterval
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        
+        result.append(newInterval)  # Add the merged interval
+        
+        # Add remaining intervals
+        while i < n:
+            result.append(intervals[i])
+            i += 1
+        
+        return result
