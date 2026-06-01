e193c0e45781213d1018f023e37a2ca980fa1d2c
deepika <maligadeepika@gmail.com>
2026-06-01 19:40:41 +0530

    update code

--- Day31.py
+++ Day31.py
@@ -1234,3 +1234,29 @@ class Solution:
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
         
         return dp[-1][-1]
+    
+#unique path2
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        
+        if obstacleGrid[0][0] == 1:
+            return 0
+        
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1
+        
+        for i in range(1, m):
+            dp[i][0] = 0 if obstacleGrid[i][0] == 1 else dp[i - 1][0]
+        
+        for j in range(1, n):
+            dp[0][j] = 0 if obstacleGrid[0][j] == 1 else dp[0][j - 1]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        
+        return dp[-1][-1]
