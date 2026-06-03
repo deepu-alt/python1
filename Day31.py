2071b1bc288b725464877b176e5e82229d31898a
deepu_123 <maligadeepika@gmail.com>
2026-06-03 18:25:27 +0530

    Implement plusOne method in Solution class
    Added a plusOne method to the Solution class that increments a list of digits representing a number.

--- Day31.py
+++ Day31.py
@@ -1291,3 +1291,14 @@ class Solution:
                 return False
         
         return num_seen
+#plus one
+class Solution:
+    def plusOne(self, digits):
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+
+            digits[i] = 0
+
+        return [1] + digits
