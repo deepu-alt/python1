5537d700d29bcadaf7df986fee093a2d41489dfc
deepu_123 <maligadeepika@gmail.com>
2026-06-05 20:53:14 +0530

    Implement text justification in Solution class
    Added a fullJustify method to the Solution class for text justification.

--- Day32.py
+++ Day32.py
@@ -33,3 +33,49 @@ class Solution:
 # 👇 MUST CALL FUNCTION
 sol = Solution()
 print("Result:", sol.isMatch("aab", "c*a*b"))
+
+#text justification 
+class Solution:
+    def fullJustify(self, words, maxWidth):
+        res = []
+        i = 0
+
+        while i < len(words):
+            line_words = []
+            line_length = 0
+
+            # Collect words for current line
+            while (i < len(words) and
+                   line_length + len(words[i]) + len(line_words) <= maxWidth):
+                line_words.append(words[i])
+                line_length += len(words[i])
+                i += 1
+
+            # Last line or single word line
+            if i == len(words) or len(line_words) == 1:
+                line = " ".join(line_words)
+                line += " " * (maxWidth - len(line))
+                res.append(line)
+
+            else:
+                total_spaces = maxWidth - line_length
+                gaps = len(line_words) - 1
+
+                even_spaces = total_spaces // gaps
+                extra_spaces = total_spaces % gaps
+
+                line = ""
+
+                for j in range(gaps):
+                    line += line_words[j]
+
+                    spaces = even_spaces
+                    if j < extra_spaces:
+                        spaces += 1
+
+                    line += " " * spaces
+
+                line += line_words[-1]
+                res.append(line)
+
+        return res
