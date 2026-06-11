d2c7ea3130dd83374bb76a577f87a1f68a91f8f2
deepu_123 <maligadeepika@gmail.com>
2026-06-11 20:30:43 +0530

    Add binary search function to search in matrix

--- Day32.py
+++ Day32.py
@@ -198,3 +198,27 @@ class Solution:
         if first_col_zero:
             for i in range(m):
                 matrix[i][0] = 0
+#search 2d matrix 
+class Solution:
+    def searchMatrix(self, matrix, target):
+        m, n = len(matrix), len(matrix[0])
+
+        left, right = 0, m * n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            # Convert 1D index to 2D index
+            row = mid // n
+            col = mid % n
+
+            value = matrix[row][col]
+
+            if value == target:
+                return True
+            elif value < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
