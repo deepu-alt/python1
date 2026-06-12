60ce8757b613a5ede0da0b71e6827023237f0d2d
deepu_123 <maligadeepika@gmail.com>
2026-06-12 21:01:54 +0530

    Add function to sort colors in an array
    Implemented the Dutch National Flag algorithm to sort an array of colors represented by 0s, 1s, and 2s in-place.

--- Day32.py
+++ Day32.py
@@ -222,3 +222,22 @@ class Solution:
                 right = mid - 1
 
         return False
+# sort colors 
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+
+            elif nums[mid] == 1:
+                mid += 1
+
+            else:  # nums[mid] == 2
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
